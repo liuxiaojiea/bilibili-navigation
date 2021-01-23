19125417998d825b46b2f081f1bcd469040ea81a
liuxja <liuxj@geovis.com.cn>
2021-01-23 18:59:57 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,37 @@
+
+let mapList = [{
+    id: 'guochuang',
+    text: '国创',
+    top: 0,
+    active: false
+}, {
+    id: 'yinyue',
+    text: '音乐',
+    top: 0,
+    active: false
+}, {
+    id: 'fanju',
+    text: '番剧',
+    top: 0,
+    active: false
+}, {
+    id: 'manhua',
+    text: '漫画',
+    top: 0,
+    active: false
+}, {
+    id: 'wudao',
+    text: '舞蹈',
+    top: 0,
+    active: false
+}, {
+    id: 'donghua',
+    text: '动画',
+    top: 0,
+    active: false
+}, {
+    id: 'zixun',
+    text: '资讯',
+    top: 0,
+    active: false
+}];
